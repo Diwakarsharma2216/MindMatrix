a86df6fa3379d43c1e1398dafde7ae4f0a84cea3
Bhavikagire <bhavikagire3035@gmail.com>
2023-06-23 11:57:59 +0530

    added login and register backend code

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 const express=require("express")
 const {Configuration,OpenAIApi} =require("openai")
+
+const {userRouter} = require("./routers/userrote")
+const {authenticate}=require("./middlewear/authentication")
+
+const {connection}=require("./db")
+
+
+
+
 const bodyParser =require("body-parser")
 require("dotenv").config()
 const cors=require('cors')
+
 
 const configuration=new Configuration({
     organization:process.env.organization,
@@ -15,7 +25,14 @@ const app=express()
 app.use(bodyParser.json())
 app.use(cors())
 app.use(express.json())
+app.use("/users",userRouter)
 
+// app.use(authenticate)
+
+
+app.get("/",(req,res)=>{
+    res.send("welcome to mindmatrix")
+})
 
 // ##### OpenAi 😊 ########
 app.post("/ask", async (req, res) => {
@@ -40,7 +57,12 @@ try {
 	
 });
 
-
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is Running at ${process.env.PORT}`)
+app.listen(process.env.PORT,async()=>{
+    try {
+        await connection
+        console.log("connected with database")
+    } catch (error) {
+        console.log(error)
+    }
+    console.log(`server is running at ${process.env.PORT} port `)
 })
